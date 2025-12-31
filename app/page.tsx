8c016358b79476b933760b532d884c4c9cb3e555
wizzzzzzzard <toil.nakul@gmail.com>
2026-01-01 00:25:56 +0530

    Add test playbook

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,4 @@
-"use client";
-import { useState } from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import YearCounter from '../components/YearCounter';
-
-export default function Page() {
-  const [year, setYear] = useState(new Date().getFullYear() + 1);
-
-  return (
-    <div className="h-screen flex flex-col">
-      <Header />
-      <main className="flex-1 flex justify-center items-center">
-        <YearCounter year={year} />
-      </main>
-      <Footer />
-    </div>
-  );
-}
+---
+- hosts: all
+  tasks:
+    - debug: msg="hello"
